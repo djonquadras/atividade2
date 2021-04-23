6ec996ee0f9026540160de45ffc169b2973bbf52
Djonathan Quadras <djonquadras@gmail.com>
2021-04-22 22:00:47 -0300

    Finalização 1.8.

--- scripts/solution108.R
+++ scripts/solution108.R
@@ -27,28 +27,22 @@ solution <- function(dados){
   zeqSEC <- dados$Z/3
   zeqPRI <- (a^2)*(dados$Z)/3
   vcr <- a*vBTFase
-  ic <- complex(modulus = (dados$P3PHI)/(sqrt(3)*vATLinha*dados$FP), argument = calculaAngulo(dados$FP, dados$ADIANTADO))
+  ic <- complex(modulus = (dados$CARGA)/(sqrt(3)*vATLinha*dados$FP), argument = -calculaAngulo(dados$FP, dados$ADIANTADO))
   ve <- ic*zeqPRI + vcr
   ie = a*ic
     
-  is <- (dados$CARGA)/(sqrt(3)*dados$VL*dados$FP)
   phi <- calculaAngulo(dados$FP, dados$ADIANTADO)
-  is <- complex(modulus = is, argument = -phi)
-  vss <- dados$VL/sqrt(3)
-  vps <- is*zeqSEC + vss
 
-  vpp <- vps*a
-  vsp <- vss/a
-  ip <- is/a
-  sp <- sqrt(3)*Mod(ie)*Mod(ve)
-  fpp <- cos(Arg(ve)-Arg(ie))
+  
+  sfonte <- sqrt(3)*Mod(ie)*Mod(ve)
+  fpe <- cos(Arg(ve)-Arg(ie))
 
-  re <- (Mod(vps)-Mod(vss))/(Mod(vss))
-  pj3phi <- 3*Re(zeqSEC)*Mod(is)^2
+  re <- (Mod(ve)-Mod(vcr))/(Mod(vcr))
+  pj3phi <- 3*Re(zeqSEC)*Mod(ic)^2
   n <- (dados$CARGA)/((dados$CARGA)+(dados$P3PHI)+(pj3phi))
-  solution <- list(ZEQ = zeqSEC, a = a, zeqPRI = zeqPRI, is = is, phi = phi,
-                   vss = vss, vps = vps, vpp = vpp, vsp = vsp, ip = ip, sp = sp,
-                   fpp = fpp, re = re, n=n, pj3phi=pj3phi,
-                   ve = ve, ic = ic, vcr=vcr, ie=ie)
+  
+  solution <- list(ZEQ = zeqSEC, a = a, zeqPRI = zeqPRI, is = ic, phi = phi,
+                   se = sfonte, fpe = fpe, re = re, n=n, pj3phi=pj3phi,
+                   ve = ve, ic = ic, vcr=vcr, ie=ie,vcr=vcr, vATFase=vATFase, vATLinha=vATLinha, vBTLinha=vBTLinha, vBTFase=vBTFase)
   return(solution)
 }
