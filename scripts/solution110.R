08130f0790ba0ecc7a5fb4eb6a3ca03531dba14d
Djonathan Quadras <djonquadras@gmail.com>
2021-04-23 00:32:18 -0300

    Finalização 1.10.

--- scripts/solution110.R
+++ scripts/solution110.R
@@ -24,26 +24,62 @@ solutionToEachTrafo <- function(dados){
   a <- vATFase/vBTFase
   vcr <- a*vBTFase
   zyEqPri <- (a^2)*(dados$Z)/3
-    is <- NULL
-    ss <- NULL
+    
+  ic <- NULL
+  ie <- NULL
+  ss <- NULL
+  
+  ve <-  NULL
+  fp <- NULL
+  re <- NULL
+  n <-  NULL
+  se <- NULL
   
   sol <- list(zyEqSec = zyEqSec, vATFase = vATFase, vATLinha = vATLinha, vBTLinha = vBTLinha,
-              vBTFase = vBTFase, a = a, vcr = vcr, zyEqPri = zyEqPri, is = is, ss = ss)
+              vBTFase = vBTFase, a = a, vcr = vcr, zyEqPri = zyEqPri, ic = ic, ss = ss, se=se,
+              ve = ve, fp=fp,re=re,n=n,ie=ie)
 }
 
 solution <- function(dados1,dados2){
   solTrafo1 <- solutionToEachTrafo(dados1)
   solTrafo2 <- solutionToEachTrafo(dados2)
   
-  is <- complex(modulus = (dados1$P3PHI)/(sqrt(3)*solTrafo1$vATLinha*dados2$FP), argument = calculaAngulo(dados2$FP, dados2$ADIANTADO))
-  ie <- is*solTrafo1$a
+  ic <- complex(modulus = (dados1$P3PHI)/(sqrt(3)*solTrafo1$vATLinha*dados2$FP), argument = -calculaAngulo(dados2$FP, dados2$ADIANTADO))
+  ie <- ic*solTrafo1$a
   
-  solTrafo2$is <- (is)/(1+(solTrafo2$zyEqPri/solTrafo1$zyEqPri)) 
-  solTrafo1$is <- is - solTrafo2$is
+  solTrafo2$ic <- (ic)/(1+(solTrafo2$zyEqPri/solTrafo1$zyEqPri)) 
+  solTrafo2$ie <- solTrafo2$ic*solTrafo1$a
   
-  solTrafo1$ss <- solTrafo1$vcr*Conj(solTrafo1$is)
-  solTrafo2$ss <- solTrafo1$vcr*Conj(solTrafo2$is)
+  solTrafo1$ic <- ic - solTrafo2$ic
+  solTrafo1$ie <- solTrafo1$ic*solTrafo1$a
   
-  sol <- list(solTrafo1 = solTrafo1, solTrafo2 = solTrafo2, solCarga = list(is = is, ie = ie))
+  solTrafo1$ss <- solTrafo1$vcr*Conj(solTrafo1$ic)
+  solTrafo2$ss <- solTrafo1$vcr*Conj(solTrafo2$ic)
+  Zpar <- (1/((1/solTrafo1$zyEqPri)+(1/solTrafo2$zyEqPri)))
+  
+  ve <- Zpar*ie + solTrafo1$vcr
+  
+  solTrafo1$ve <- solTrafo1$zyEqPri*solTrafo1$ie + solTrafo1$vcr
+  solTrafo2$ve <- solTrafo2$zyEqPri*solTrafo2$ie + solTrafo1$vcr
+  
+  solTrafo1$fp <- cos(Arg(solTrafo1$ve)-Arg(solTrafo1$ie))
+  solTrafo2$fp <- cos(Arg(solTrafo2$ve)-Arg(solTrafo2$ie))
+  
+  solTrafo1$se <- sqrt(3)*Mod(solTrafo1$ve)*Mod(solTrafo1$ie)
+  solTrafo2$se <- sqrt(3)*Mod(solTrafo2$ve)*Mod(solTrafo2$ie)
+  
+  solTrafo1$re <- (Mod(solTrafo1$ve)-Mod(solTrafo1$vcr))/Mod(solTrafo1$vcr)
+  solTrafo2$re <- (Mod(solTrafo2$ve)-Mod(solTrafo2$vcr))/Mod(solTrafo2$vcr)
+  
+  solTrafo2$n <- 1-Mod((solTrafo2$ss*dados2$FP)/(solTrafo2$se*solTrafo2$fp))
+  solTrafo1$n <- 1-Mod((solTrafo1$ss*dados1$FP)/(solTrafo1$se*solTrafo1$fp))
+  
+  sfonte <- sqrt(3)*Mod(ve)*Mod(ie)
+  fpfonte <- cos(Arg(ve)-Arg(ie))
+  
+
+  
+  sol <- list(solTrafo1 = solTrafo1, solTrafo2 = solTrafo2, solCarga = list(ic = ic, ie = ie, ve=ve, Zpar=Zpar,
+                                                                            sfonte=sfonte,fpfonte=fpfonte))
 
   }
